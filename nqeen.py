d300e4658ff7cb185d1b0ed20b575d6727e66472
phanthomx <67103893+phanthomx@users.noreply.github.com>
2023-08-14 22:52:52 +0530

    Add files via upload

--- nqeen.py
+++ nqeen.py
@@ -31,8 +31,8 @@ def clear_pathways(l,a,b,brd):
     except:
         pass
     brd[m][n] = "Q"
-    return brd
-def clearp(l,m,n,brd):
+
+
     try:
         for i in range(min(m,n)+1):
             brd[m-i][n+i]='1'
@@ -43,16 +43,14 @@ def clearp(l,m,n,brd):
 def nqueen(l,m,n):
     brd=make_grid(l)
     brd = clear_pathways(l, m, n, brd)
-    brd=clearp(l,m,n,brd)
     for i in range(l):
         for j in range(l):
             if brd[i][j]=='0':
-                brd = clearp(l, i, j, brd)
                 brd = clear_pathways(l, i, j, brd)
     return brd
-for ii in range(5):
-    for jj in range(5):
-        z = nqueen(5, ii, jj)
+for ii in range(4):
+    for jj in range(4):
+        z = nqueen(4, ii, jj)
         for i in z:
             print(i, end="\n")
         print("__________________")
