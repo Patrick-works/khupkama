0509d5a911cacab1b9e5c9475826974818c3111b
Patrick batman <138879144+Patrick-works@users.noreply.github.com>
2024-02-24 10:15:52 +0530

    Add files via upload

---
+++ AryanPra/po3.R
@@ -0,0 +1,12 @@
+library(party)
+print(head(readingSkills))
+png(file = "decision_tree.png")
+plot(output.tree) 
+# Save the file. 
+dev.off() 
+# Plot the tree. 
+plot(output.tree)plot(output.tree) 
+# Save the file. 
+dev.off() 
+# Plot the tree. 
+plot(output.tree)
